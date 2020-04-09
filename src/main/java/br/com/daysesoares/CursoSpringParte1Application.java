58c2786f99218d5a4dea29789989076c2a781455
Dayse Soares <daysedesign@gmail.com>
2020-04-08 21:33:23 -0300

    Criacao do primeiro projeto em spring

---
+++ src/main/java/br/com/daysesoares/CursoSpringParte1Application.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringParte1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringParte1Application.class, args);
+	}
+
+}
