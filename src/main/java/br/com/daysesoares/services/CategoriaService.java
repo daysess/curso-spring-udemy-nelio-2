53a34217bbd3ce0213d2795f495bd84fffb48618
Dayse Soares <daysedesign@gmail.com>
2020-04-20 20:56:52 -0300

    Paginacao com parametros opcionais

--- src/main/java/br/com/daysesoares/services/CategoriaService.java
+++ src/main/java/br/com/daysesoares/services/CategoriaService.java
@@ -2,9 +2,11 @@ package br.com.daysesoares.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.daysesoares.domain.Categoria;
@@ -47,6 +49,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 }
