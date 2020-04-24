8cb35f3b8a88c4a3eb83f3da86ef3cfbeb8edbde
Dayse Soares <daysedesign@gmail.com>
2020-04-23 21:11:17 -0300

    validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/br/com/daysesoares/repository/ClienteRepository.java
+++ src/main/java/br/com/daysesoares/repository/ClienteRepository.java
@@ -1,9 +1,14 @@
 package br.com.daysesoares.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.daysesoares.domain.Cliente;
 
 public interface ClienteRepository  extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/br/com/daysesoares/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/daysesoares/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.daysesoares.domain.Cliente;
 import br.com.daysesoares.domain.enums.TipoCliente;
 import br.com.daysesoares.dto.ClienteNewDTO;
+import br.com.daysesoares.repository.ClienteRepository;
 import br.com.daysesoares.resources.exception.FieldMessage;
 import br.com.daysesoares.services.validation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if(cliente != null) {
+			list.add(new FieldMessage("email","Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
