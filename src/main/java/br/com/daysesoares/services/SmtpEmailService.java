6526074609a9b1f1e3dbd5a232210c328ffb16e3
Dayse Soares <daysedesign@gmail.com>
2020-04-30 21:33:45 -0300

    Implementando SmtpEmailService com servidor do Google

---
+++ src/main/java/br/com/daysesoares/services/SmtpEmailService.java
@@ -0,0 +1,23 @@
+package br.com.daysesoares.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+
+	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Autowired
+	private MailSender mailSender;
+	
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Simulando envio de email...");
+		mailSender.send(msg);
+		LOG.info("Email enviado.");
+		
+	}
+
+}
