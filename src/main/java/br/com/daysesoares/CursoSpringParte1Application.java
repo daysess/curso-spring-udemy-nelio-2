faf6e002874d26321680d5843eaa7e522bafe015
Dayse Soares <daysedesign@gmail.com>
2020-05-13 21:13:58 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/daysesoares/CursoSpringParte1Application.java
+++ src/main/java/br/com/daysesoares/CursoSpringParte1Application.java
@@ -1,12 +1,17 @@
 package br.com.daysesoares;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.daysesoares.services.S3Service;
+
 @SpringBootApplication
 public class CursoSpringParte1Application implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringParte1Application.class, args);
@@ -14,7 +19,7 @@ public class CursoSpringParte1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\temp\\foto\\PEC108.JPG");
 	}
 
 }
